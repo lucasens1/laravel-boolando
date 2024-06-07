6874cd496ba2a78c20c9b772eb33fbcb78861044
lucasens1 <lucasensini1@gmail.com>
2024-06-07 22:23:24 +0200

    implemented logic using @php blade directive

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,8 +1,41 @@
-{{-- <h1 class="mb-5"> TEST ITERAZIONE </h1> --}}
-
 <div class="ms_card d-flex flex-column">
+
+    {{-- Inserimento dell'immagine --}}
     <img src="{{ Vite::asset('resources/img/' . $item_desc['frontImage']) }}" alt="">
-    <p class="ms_fs-small">{{$item_desc['brand']}}</p class="ms_fs-small">
-    <p class="fw-semibold">{{ $item_desc['name']}}</p>
-    <span> {{ $item_desc['price'].' €' }} </span>
+    {{-- Brand --}}
+    <p class="ms_fs-small">{{ $item_desc['brand'] }}</p>
+    {{-- Nome --}}
+    <p class="fw-semibold">{{ $item_desc['name'] }}</p>
+    {{-- 
+        Uso la direttiva Blade @php che mi permette di creare una finestra di codice php, così da creare flag e discountedPrice 
+    --}}
+    @php
+        $isDiscounted = false;
+        $discountedPrice = $item_desc['price'];
+    @endphp
+    {{-- Itero sui badges in $item_desc --}}
+    @foreach ($item_desc['badges'] as $badge)
+        {{--
+            Controllo se type è settato e se ha la voce 'discount' 
+        --}}
+        @if (isset($badge['type']) && $badge['type'] === 'discount')
+            {{-- 
+            Entrando nella condizione posso quindi usare la flag -> true, e calcolare il prezzo 
+        --}}
+            @php
+                # Rimuovo sia il simbolo percentuale che il segno meno, passando [% e -]
+                $discountPercentage = str_replace(['%', '-'], '', $badge['value']);
+                $discountedPrice = $item_desc['price'] * ($discountPercentage / 100);
+                $isDiscounted = true;
+            @endphp
+            <p>
+                <span class="ms_primary-price fw-bold ms_fs-small"> {{ number_format($discountedPrice, 2) }} € </span>
+                <span class="fw-bold ms_fs-small"> <del>{{ $item_desc['price'] . ' €' }}</del></span>
+            </p>
+        @endif
+    @endforeach
+    {{-- Visualizzo il prezzo normale se non è settato discount  --}}
+    @if (!isset($isDiscounted))
+        <span class="fw-bold ms_fs-small">{{ $item_desc['price'] . ' €' }}</span>
+    @endif
 </div>
