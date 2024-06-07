5d6176af367bd1733aed822ea978d8617114e00c
lucasens1 <lucasensini1@gmail.com>
2024-06-07 20:30:39 +0200

    added : header, un po' lazialotto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@include('partials.header')
 @section('content')
     <h1>Home page</h1>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
-<header>
-    Header
+<header class="container-fluid pt-3 align-items-center">
+    <div class="container d-flex justify-content-between">
+        <ul class="d-flex gap-4">
+            <li>Uomo</li>
+            <li>Donna</li>
+            <li>Bambino</li>
+        </ul>
+        <img src="{{ Vite::asset('resources/img/boolean-logo.png')}}" alt="">
+
+        <ul class="d-flex gap-4">
+            <li>Omino</li>
+            <li>Cuore</li>
+            <li>Carrello</li>
+        </ul>
+    </div>  
 </header>
