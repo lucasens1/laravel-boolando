49d956842f3fd1868ff3a361f29538bfd4df1642
lucasens1 <lucasensini1@gmail.com>
2024-06-07 21:17:13 +0200

    created home cycle for card creation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
+
 @include('partials.header')
+
 @section('content')
-    <h1>Home page</h1>
+    <div class="container ms_pt-120">
+        <div class="row flex-row">
+        {{-- @foreach in Homepage @forelse in Uomo/Donna/Bambino in caso di non presenza di prodotti --}}
+            @foreach ($products as $item)
+                @foreach ($item as $item_desc)
+                    <div class="col-4">
+                        {{-- @dd($item_desc) --}}
+                        @include('partials.card')
+                    </div>
+                @endforeach
+            @endforeach
+        </div>
+    </div>
 @endsection
---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,5 @@
+{{-- <h1 class="mb-5"> TEST ITERAZIONE </h1> --}}
+
+<div class="ms_card text-center align-items-center">
+    {{$item_desc['id']}}
+</div>
